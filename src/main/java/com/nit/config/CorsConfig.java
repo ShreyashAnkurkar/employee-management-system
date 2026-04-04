50dfa6e7a4fab8e8eb3e6db8eaac8465db90dd87
Shreyash Ankurkar <144425421+ShreyashAnkurkar@users.noreply.github.com>
2026-04-04 17:42:50 +0530

    Update CorsConfig.java

--- src/main/java/com/nit/config/CorsConfig.java
+++ src/main/java/com/nit/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of( "http://localhost:5173",
-        	    "https://ems-frontend.vercel.app"));
+        config.setAllowedOrigins(List.of(  "http://localhost:5173",
+        	    "https://ems-frontend-production.up.railway.app"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
